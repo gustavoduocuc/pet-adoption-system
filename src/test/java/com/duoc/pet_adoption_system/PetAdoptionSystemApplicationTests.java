1e82b8d05b82bcae59cb05352b82dcc9d98f5f06
Gustavo Dominguez <gus.dominguez@duocuc.cl>
2026-03-24 18:46:16 -0300

    Initial configuration

---
+++ src/test/java/com/duoc/pet_adoption_system/PetAdoptionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.pet_adoption_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
